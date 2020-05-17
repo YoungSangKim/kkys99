62d4a822c4f7750dae1f9cef5aeee2c1b8f3a231
YoungSangKim <kkys96@gmail.com>
2020-05-17 17:45:53 +0900

    Add files via upload

---
+++ Algo/BOJ/n14681_20.05.17.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class n14681 {
+
+	static int x, y;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		x = Integer.parseInt(br.readLine());
+		y = Integer.parseInt(br.readLine());
+		if (x > 0 && y > 0)
+			System.out.println(1);
+		else if (x < 0 && y > 0)
+			System.out.println(2);
+		else if (x < 0 && y < 0)
+			System.out.println(3);
+		else
+			System.out.println(4);
+	}
+}
