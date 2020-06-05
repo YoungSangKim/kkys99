f57c6c1ab9933bbd1ea4335754c2d0984aa27227
YoungSangKim <kkys96@gmail.com>
2020-06-05 20:38:47 +0900

    Add files via upload

---
+++ Algo/BOJ/n15663_20.06.05.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class n15663 {
+	static int N, M;
+	static int[] arr, result;
+	static boolean[] visit;
+	static LinkedHashSet<String> set;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		result = new int[M];
+		visit = new boolean[N];
+		set = new LinkedHashSet<>();
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		dfs(0, "");
+		for (String s : set)
+			System.out.println(s.trim());
+	}
+
+	private static void dfs(int depth, String s) {
+		// TODO Auto-generated method stub
+		if (depth == M) {
+			set.add(s);
+			return;
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			if (visit[i])
+				continue;
+			visit[i] = true;
+			dfs(depth + 1, s + arr[i] + " ");
+			visit[i] = false;
+		}
+	}
+}
